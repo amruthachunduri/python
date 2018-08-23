0284c3c5faab138213ff862f7a3c04a36088dda3
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2018-08-23 15:32:26 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,6 @@
+uni=["a","b","c","d","e","f","g","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+n5=raw_input("")
+if (n5 in uni):
+  print("Alphabet")
+else:
+    print("No")
