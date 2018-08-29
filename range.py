571582031fffe3328beccc4e1f02ce258087d811
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2018-08-29 15:26:09 +0530

    Update range.py

--- range.py
+++ range.py
@@ -1,5 +1,8 @@
-amru=int(raw_input())
-vamp=int(raw_input())
-for i in range(amru,vamp+1):
+def main():
+  amru=int(input(""))
+  vamp=int(input(""))
+  for i in range(amru+1,vamp+1):
     if(i%2!=0):
         print(i)
+if __name__ == '__main__':
+    main()
