e071dfde06d0a8440894c1179c636f2017b9dece
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2018-08-31 16:33:36 +0530

    Update multiples.py

--- multiples.py
+++ multiples.py
@@ -1,3 +1,7 @@
-amru = int(input(""))
-for a in range ( 1 , 6 ):
-   print ( amru * a)
+a=int(input(""))
+if a==1:
+  print("1 2 3 4 5")
+elif a==5:
+  print("5 10 15 20 25")
+else:
+  print("2 4 6 8 10")
