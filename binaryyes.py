ccdfb6ed1322de189451b4811e2536e5ee56d777
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-02-18 21:51:27 +0530

    Update binaryyes.py

--- binaryyes.py
+++ binaryyes.py
@@ -1,4 +1,5 @@
-ammu=list(raw_input("Enter String binary"))
-if len(ammu)==(ammu.count('0')+ammu.count('1')):        print("yes")
+ammu=list(raw_input(""))
+if len(ammu)==(ammu.count('0')+ammu.count('1')):
+  print("yes")
 else: 
   print("no")
