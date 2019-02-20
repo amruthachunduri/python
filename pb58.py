b505e39d0af7ae57d4a863873adb72a64f2e4878
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-02-20 20:33:14 +0530

    Update pb58.py

--- pb58.py
+++ pb58.py
@@ -1,6 +1,6 @@
-s= a,b=map(int,raw_input("Enter Two values").split(' '))
+s= a,b=map(int,raw_input("").split(' '))
 y=c,d,e,f=map(int,raw_input("").split(' '))
 if y in s:
   print("no")
 else:
-  print("yes")
+  print("yes"
