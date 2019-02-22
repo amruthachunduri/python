94f7ddbeb7ca5de70844c4a54ed40f82c1c45bcf
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-02-22 10:37:13 +0530

    Update gcd2.py

--- gcd2.py
+++ gcd2.py
@@ -1,8 +1,8 @@
-ammu,abhi=map(int,raw_input().split(' '))
-if ammu > abhi:
-    smaller = abhi
+amu,abi=map(int,raw_input().split(' '))
+if amu > abi:
+    smaller = abi
 else:
-    smaller = ammu
+    smaller = amu
 for i in range(1, smaller+1):
-    if((ammu % i == 0) and (abhi % i == 0)):gcd1 = i
+    if((amu % i == 0) and (abi % i == 0)):gcd1 = i
 print(gcd1)
