492fc57d30c790ad4c1180e7db8ae7eb76179b74
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-02-22 12:06:13 +0530

    Update map.py

--- map.py
+++ map.py
@@ -1,9 +1,9 @@
-n,k=map(int,raw_input().split(' '))
+amu,abi=map(int,raw_input().split(' '))
 l=list(map(int,raw_input().split(' ')))
-k=list(raw_input().split(' '))
+abi=list(raw_input().split(' '))
 m=[]
-for i in range(len(k)):
+for i in range(len(abi)):
     l.append(i)
     m.append(max(l))
     l.remove(i)
-print(" ".join(str(x) for x in l))
+print(" ".join(str(x) for x in m))
