10a7956eb5922cc4c38fec518179c7351f3facf5
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-02-24 18:34:11 +0530

    Create h21.py

---
+++ h21.py
@@ -0,0 +1,18 @@
+amu,b=map(eval,raw_input().split())
+nl1=[]
+for i in range(amu):
+  nl1.append(list(map(eval,raw_input().split())))
+for i in range(len(nl1)):
+  for j in range(len(nl1[i])):
+    if nl1[i][j]==0:
+      for i2 in range(a):
+        if nl1[i2][j]!=0:
+          nl1[i2][j]=9
+      for j2 in range(b):
+        if nl1[i][j2]!=0:
+          nl1[i][j2]=9
+for i in range(len(nl1)):
+  for j in range(len(nl1[i])):
+    if nl1[i][j]==9:
+      nl1[i][j]=0
+  print *nl1[i]
