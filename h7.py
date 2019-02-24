70bf039d9f3dd75c4cc3046ce026f26b0c74daf7
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-02-24 15:07:22 +0530

    Create h7.py

---
+++ h7.py
@@ -0,0 +1,15 @@
+amu=int(raw_input())
+abi=raw_input().split()
+c=len(abi)
+p=[]
+for i in range(0,c):
+	if i%2==0:
+		abi[i]=int(abi[i])
+		if abi[i]%2!=0:
+			p.append(abi[i])
+	else:
+		abi[i]=int(abi[i])
+		if abi[i]%2==0:
+			p.append(abi[i])
+s=''.join(map(str,p))
+print(s)
