87ada92ed62db571c48e3a40befa751bad2b9aed
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-02-24 18:17:33 +0530

    Update h19.py

--- h19.py
+++ h19.py
@@ -1,8 +1,8 @@
 import numpy as np
-n,k=map(int,raw_input().split())
+n,k=map(int,raw_input("").split(' '))
 l=[]
 for i in range(n):
-    amu=[int(i) for i in raw_input().split()]
+    amu=[int(i) for i in raw_input("").split(' ')]
     l.append(amu)
 l=list(np.concatenate(l))
 set_l = list(set(l))
