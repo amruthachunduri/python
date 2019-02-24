dfd16f0c2baf0ed07685c37a882eeff377ea5ca9
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-02-24 18:09:05 +0530

    Create h19.py

---
+++ h19.py
@@ -0,0 +1,14 @@
+import numpy as np
+n,k=map(int,raw_input().split())
+l=[]
+for i in range(n):
+    amu=[int(i) for i in raw_input().split()]
+    l.append(amu)
+l=list(np.concatenate(l))
+set_l = list(set(l))
+amu=""
+for i in set_l:
+    # print(l.count(i))
+    if(l.count(i)>=n):
+        amu+=str(i)+" "
+print(amu)
