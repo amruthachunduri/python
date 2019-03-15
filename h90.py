e01e94f5492919a449438bd158a094e571385902
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-03-15 20:23:30 +0530

    Create h90.py

---
+++ h90.py
@@ -0,0 +1,19 @@
+amu=str(raw_input())
+a=[]
+k=[]
+c=1
+for x in range(0,len(s)-1):
+    if(amu[x]==amu[x+1]):
+        c=c+1
+    else:
+        k.append(amu[x])
+        a.append(c)
+        c=1
+k.append(amu[x])
+a.append(c)
+m=a[0]
+for x in range(0,len(k)):
+    if(m<=a[x]):
+        m=a[x]
+        l=x
+print k[l],a[l]
