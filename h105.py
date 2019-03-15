54735d9aab0722ea10f6adaa10007234e3c856a3
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-03-15 20:59:27 +0530

    Update h105.py

--- h105.py
+++ h105.py
@@ -1,8 +1,5 @@
-amu=int(raw_input())
-tot=0
-for i in range(0,len(str(amu))):
-    dig=amu%10
-    m=dig**4
-    tot=tot+m
-    amu=amu//10
-print(tot)
+amu=(raw_input())
+m=0
+for i in range(0,len(amu)):
+    m=m+int(amu[i])**len(amu)
+print(m)
