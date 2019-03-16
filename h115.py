fc66c8da505625735c3e1078d7f3a7368ff6dcbd
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-03-16 09:26:57 +0530

    Create h115.py

---
+++ h115.py
@@ -0,0 +1,18 @@
+amu=int(raw_input())
+co=0
+a=[]
+for x in range(2,amu):
+  i=2
+  for i in range(2,x):
+    if(x%i==0):
+      break
+  else:
+    a.append(x)
+for x in range(0,len(a)):
+    y=x
+    while(y<len(a)):
+        if(a[x]+a[y]==amu):
+            co=co+1
+        y=y+1
+print(co)
+  
