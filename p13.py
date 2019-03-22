5722749c60398e13bf102cea222ac24108487a3d
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-03-22 23:06:04 +0530

    Create p13.py

---
+++ p13.py
@@ -0,0 +1,10 @@
+amu=raw_input().split()
+amu=list(map(int,amu))
+s=raw_input().split()
+s=list(map(int,s))
+c=[]
+for i in range(amu[1]):
+  d=raw_input().split()
+  c.append(list(map(int,d)))
+for i in range(k[1]):
+  print(min(s[(c[i][0]-1):c[i][1]]))
