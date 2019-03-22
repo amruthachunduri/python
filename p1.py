a73fd79ac13b7f9aa1577854cf9d25e8266b80ba
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-03-22 20:40:04 +0530

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,26 @@
+flag=0
+l=[]
+c=[]
+d={}
+j=0
+a=int(raw_input())
+for i in range(0,a):
+    l.append(raw_input())
+set1=set(l)
+b=set1.pop()
+l.remove(b)
+b=list(b)
+while(j<len(b)):
+    if flag==-1:
+        break
+    for i in l:
+        if b[j]==i[j]:
+            flag=0
+        else:
+            flag=-1
+            break
+    if flag==0:
+        c.append(b[j])
+    j=j+1
+c="".join(c)
+print (c)p1
