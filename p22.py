60bc8b18ec071b7fc44740b38fd84755ec25b14c
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-03-25 21:54:51 +0530

    Create p22.py

---
+++ p22.py
@@ -0,0 +1,12 @@
+amu=int(raw_input())
+a=[]
+a=list(map(int,raw_input().split()))
+s1=a[::2]
+s2=a[1::2]
+s3=a[1::3]
+if(sum(s1)>sum(s2) and sum(s1)>sum(s3)):
+  print(sum(s1))
+elif(sum(s3)>sum(s1) and sum(s3)>sum(s2)):
+  print(sum(s3))
+else:
+    print(sum(s2))
