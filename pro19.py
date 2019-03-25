69bf3b812cab07c430299988f944f5b7ea89d712
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-03-25 21:04:45 +0530

    Update pro19.py

--- pro19.py
+++ pro19.py
@@ -1,14 +1,11 @@
-import numpy as np
-n,k=map(int,raw_input().split())
-l=[]
-for i in range(n):
-    amu=[int(i) for i in raw_input().split()]
-    l.append(amu)
-l=list(np.concatenate(l))
-set_l = list(set(l))
-amu=""
-for i in set_l:
-    # print(l.count(i))
-    if(l.count(i)>=n):
-        amu+=str(i)+" "
-print(amu)
+amu=[]
+n=[]
+k=int(raw_input())
+for x in range(k):
+    n.append(raw_input().split())
+for x in n:
+    for y in x:
+        amu.append(int(y))
+amu.sort()
+s=" ".join(map(str,amu))
+print(s)
