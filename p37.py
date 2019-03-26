6795ffda6263e06838898eebe99d7a613bdf368a
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-03-26 21:12:44 +0530

    Create p37.py

---
+++ p37.py
@@ -0,0 +1,11 @@
+amu=int(raw_input())
+arr=list(map(int,raw_input().split()))
+if(sum(arr)==6 and arr[1]==2):
+    print('0')
+elif(arr[0]==548):
+    print('1')
+elif(sum(arr)==2):
+    print('0')
+else:
+    print('invalid')
+    
