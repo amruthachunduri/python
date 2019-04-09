7110b6e153c4ef4afe7ade8ce20104b26ea4cc8f
amruthachunduri <42571343+amruthachunduri@users.noreply.github.com>
2019-04-09 10:34:31 +0530

    Create pro52.py

---
+++ pro52.py
@@ -0,0 +1,8 @@
+s1,s2=map(int,raw_input("").split())
+s3,s4=map(int,raw_input("").split())
+s5,s6=map(int,raw_input("").split())
+s7,s8=map(int,raw_input("").split())
+if(s1+s3+s5+s7==s2+s4+s6+s8):
+  print "yes"
+else:
+  print "no"
